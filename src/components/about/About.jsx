a5c91cc782cf50a029f52f65e4392ce6599d34d1
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-24 12:45:50 +0400

    create new component about

---
+++ src/components/about/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./About.css"
+
+
+
+
+const About = () => {
+  return (
+    <>
+      <h1>about</h1>
+    </>
+  )
+}
+
+export default About
--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -3,6 +3,7 @@ import "./Pages.css"
 import { Route, Routes, Router, Switch } from "react-router-dom";
 import Header from '../components/common/header/Header';
 import Home from "../components/home/Home"
+import About from "../components/about/About"
 
 const Pages = () => {
     return (
@@ -10,6 +11,7 @@ const Pages = () => {
             <Header />
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
             </Routes>
         </>
     )
