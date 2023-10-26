c17424781f917e9fb505da2603c3300c46c03804
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-26 19:07:08 +0400

    add css codes for portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Portfolio.css"
+import { portfolio } from '../../data/data'
+import { MdVisibility } from "react-icons/md";
 
 const Portfolio = () => {
-  return (
-    <>
-      <h1>portfolio</h1>
-    </>
-  )
+
+    const [list, setList] = useState(portfolio)
+
+    return (
+        <>
+            <article>
+                <div className="container">
+                    <div className="btnCta">
+                        text
+                    </div>
+
+
+                    <div className="content grid3">
+                        {list.map((items) => (
+                            <div className="box">
+                                <div className="img">
+                                    <img src={items.cover} alt="" />
+                                </div>
+
+                                <div className="overlay">
+                                    <h3>{items.title}</h3>
+                                    <span>{items.name}</span>
+
+                                    <MdVisibility />
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </article>
+        </>
+    )
 }
 
 export default Portfolio
