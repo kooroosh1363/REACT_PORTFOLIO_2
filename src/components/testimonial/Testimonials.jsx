9b65d53a9035d34c847f4ce60d0142a080489fbf
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-27 12:16:59 +0400

    add css codes for test component and slick slider

--- src/components/testimonial/Testimonials.jsx
+++ src/components/testimonial/Testimonials.jsx
@@ -2,32 +2,46 @@ import React from 'react'
 import "./Testmonials.css"
 import { testimonials } from '../../data/data'
 import { BiSolidQuoteAltRight } from "react-icons/bi";
-
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Testimonials = () => {
-  return (
-    <>
-      <div className="testimonials hero">
-        <div className="container">
-            {testimonials.map((val)=>(
-                <div className="box">
-                    <i>
-                        <BiSolidQuoteAltRight/>
-                        <p>{val.text}</p>
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+    };
+    
+    return (
+        <>
+            <div className="testimonials hero">
+                <div className="container">
+                    <Slider {...settings}>
+                        {testimonials.map((val) => (
+                            <div className="box">
+                                <i>
+                                    <BiSolidQuoteAltRight />
+                                </i>
+                                <p>{val.text}</p>
 
-                        <div className="img">
-                            <img src={val.image} alt="" />
-                        </div>
+                                <div className="img">
+                                    <img src={val.image} alt="" />
+                                </div>
 
-                        <h3>{val.name}</h3>
-                        <label>{val.post}</label>
-                    </i>
+                                <h3>{val.name}</h3>
+                                <label>{val.post}</label>
+
+                            </div>
+                        ))}
+                    </Slider>
+
                 </div>
-            ))}
-        </div>
-      </div>
-    </>
-  )
+            </div>
+        </>
+    )
 }
 
 export default Testimonials
