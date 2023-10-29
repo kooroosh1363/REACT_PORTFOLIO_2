ad2c6519393dad4b9f9a73e940a7c13141914e8d
kooroosh1363 <peymanraad1363@gmail.com>
2023-10-29 23:13:38 +0400

    add css codes and html codes for contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
+import Heading from '../common/heading/Heading';
+import "./Contact.css"
+import { contact } from '../../data/data'
+
 
 const Contact = () => {
   return (
-    <div>
-      contact
-    </div>
+    <>
+      <div className="contact">
+        <div className="container">
+            <Heading title="Contact US"/>
+
+            <div className="content sbFlex">
+                <div className="right">
+                    <form>
+                        <div className="flex">
+                            <input type="text" placeholder='Name' />
+                            <input type="email" placeholder="Email" />
+                        </div>
+                        <input type="text" placeholder='Subject' />
+                        <textarea name="" id="" cols="30" rows="10"></textarea>
+                        <button>Submit</button>
+                        
+                    </form>
+                </div>
+
+                <div className="left">
+                    {contact.map((item)=>(
+                        <div className="box">
+                            <i>{item.icon}</i>
+                            <p>{item.text1}</p>
+                            <p>{item.text2}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+      </div>
+    </>
   )
 }
 
